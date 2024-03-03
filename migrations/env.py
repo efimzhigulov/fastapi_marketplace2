c47238ab7b4b29f0a72c8ef2f592432a0967ffb9
Efim <efimzhigulov@mail.ru>
2024-03-03 20:56:02 +0300

    no postgres

--- migrations/env.py
+++ migrations/env.py
@@ -13,11 +13,11 @@ from models import metadata
 config = context.config
 
 section = config.config_ini_section
-config.set_section_option(section, "DB_HOST", DB_HOST)
-config.set_section_option(section, "DB_PORT", DB_PORT)
-config.set_section_option(section, "DB_USER", DB_USER)
-config.set_section_option(section, "DB_NAME", DB_NAME)
-config.set_section_option(section, "DB_PASS", DB_PASS)
+config.set_section_option(section, "DB_HOST", 'postgres_db')
+config.set_section_option(section, "DB_PORT", '5432')
+config.set_section_option(section, "DB_USER", 'postgres')
+config.set_section_option(section, "DB_NAME", 'postgres')
+config.set_section_option(section, "DB_PASS", '2009')
 
 # Interpret the config file for Python logging.
 # This line sets up loggers basically.
